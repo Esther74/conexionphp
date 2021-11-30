fda0da99a0faa7fe2560560bb2ae000740a8adb2
Esther74 <estherhache@hotmail.com>
2021-11-30 22:55:39 +0100

    subir archivos

---
+++ conexion.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        $servername = "localhost";
+        $database = "hipopresiva";
+        $username = "root";
+        $password = "root";
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $database);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        echo "Connected successfully";
+        mysqli_close($conn);
+        
+        
+        ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>conexion a una base de datos</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo "ya funciona php";
+        
+        
+        ?>
+        <br>
+        <a href="conexion.php">Conexion</a>
+    </body>
+</html>
